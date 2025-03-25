1c21f87e4803ef1a86e117a4600fa9b64e8422b6
Obed <obekcdr@hotmail.com>
2025-03-25 15:52:26 -0700

    agrego page

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function login(): JSX.Element {
+  return (
+    <View>
+      <Text>login</Text>
+    </View>
+  );
+}
